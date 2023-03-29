dd6af7188c5af0b4cca6a229d2bfb47fdec4127a
Alessandro-Sebastiano <alesebastiano15@gmail.com>
2023-03-30 00:47:36 +0200

    properties

---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO pizzas_db.pizzas(description, name, price) VALUES('description', 'Margherita', 5);
+INSERT INTO pizzas_db.pizzas(description, name, price) VALUES('description', 'Diavola', 7);
+INSERT INTO pizzas_db.pizzas(description, name, price) VALUES('description', 'Boscaiola', 5);
