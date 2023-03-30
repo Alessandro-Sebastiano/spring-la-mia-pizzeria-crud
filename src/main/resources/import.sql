75b3ee596aa990d224ad0b9027eebe7bd4a3b50b
Alessandro-Sebastiano <alesebastiano15@gmail.com>
2023-03-30 15:41:50 +0200

    review/back-button

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,3 @@
-INSERT INTO pizzas_db.pizzas(description, name, price) VALUES('description', 'Margherita', 5);
-INSERT INTO pizzas_db.pizzas(description, name, price) VALUES('description', 'Diavola', 7);
-INSERT INTO pizzas_db.pizzas(description, name, price) VALUES('description', 'Boscaiola', 5);
+INSERT INTO pizzas_db.pizzas(description, name, price) VALUES('Pizza pomodoro, mozzarella.', 'Margherita', 5);
+INSERT INTO pizzas_db.pizzas(description, name, price) VALUES('Pizza pomodoro, mozzarella, salame piccante.', 'Diavola', 7);
+INSERT INTO pizzas_db.pizzas(description, name, price) VALUES('Pizza funghi, salsiccia, mozzarella', 'Boscaiola', 5);
