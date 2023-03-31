ba246707f7371bd84003c32369848ffed2fff34e
Alessandro-Sebastiano <alesebastiano15@gmail.com>
2023-03-31 16:04:25 +0200

    pizzas-controller-create/store

--- src/main/java/org/lessons/java/springlamiapizzeriacrud/controller/PizzaController.java
+++ src/main/java/org/lessons/java/springlamiapizzeriacrud/controller/PizzaController.java
@@ -1,14 +1,14 @@
 package org.lessons.java.springlamiapizzeriacrud.controller;
 
+import jakarta.validation.Valid;
 import org.lessons.java.springlamiapizzeriacrud.model.Pizza;
 import org.lessons.java.springlamiapizzeriacrud.repository.PizzaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
@@ -41,4 +41,26 @@ public class PizzaController {
         }
     }
 
+
+    @GetMapping("/create")
+    public String create(Model model) {
+        model.addAttribute("pizza", new Pizza());
+        return "/pizzas/create";
+    }
+
+    @PostMapping("/create")
+    public String store(@Valid @ModelAttribute("pizza") Pizza formPizza, BindingResult bindingResult, Model model) {
+
+        if (bindingResult.hasErrors()) {
+            return "/pizzas/create";
+        }
+
+        Pizza persistPizza = new Pizza();
+        persistPizza.setName(formPizza.getName());
+        persistPizza.setDescription(formPizza.getDescription());
+        persistPizza.setPrice(formPizza.getPrice());
+
+        repo.save(persistPizza);
+        return "redirect:/pizzas";
+    }
 }
