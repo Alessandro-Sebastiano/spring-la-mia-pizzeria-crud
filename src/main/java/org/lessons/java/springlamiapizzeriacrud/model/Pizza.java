c4d80ed606f9f4598a76c693995d561a9e1135a0
Alessandro-Sebastiano <alesebastiano15@gmail.com>
2023-03-31 16:36:58 +0200

    review

--- src/main/java/org/lessons/java/springlamiapizzeriacrud/model/Pizza.java
+++ src/main/java/org/lessons/java/springlamiapizzeriacrud/model/Pizza.java
@@ -1,6 +1,7 @@
 package org.lessons.java.springlamiapizzeriacrud.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PositiveOrZero;
@@ -13,6 +14,7 @@ public class Pizza {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
+    @NotBlank
     @NotEmpty
     @Column(nullable = false)
     private String name;
